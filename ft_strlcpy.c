273d35971ccc89920cf0ff2b9173370ad9f1f6c2
bifrah <benj.ifrah@gmail.com>
2021-06-25 11:03:11 +0200

    test

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bifrah <bifrah@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jamin <jamin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/17 17:27:34 by bifrah            #+#    #+#             */
-/*   Updated: 2021/06/17 17:27:37 by bifrah           ###   ########.fr       */
+/*   Updated: 2021/06/25 10:39:09 by jamin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,4 +34,20 @@ unsigned int	ft_strlcpy(char *dest, const char *src, unsigned int n)
 	dest[i] = '\0';
 	j = ft_strlen(src);
 	return (j);
+}
+
+#include <string.h>
+#include <stdio.h>
+#include "libft.h"
+
+int main()
+{
+	char src[] = "coucou";
+	char dest[10]; memset(dest, 'A', 10);
+	ft_strlcpy(dest, src, 2);
+	printf("%s\n",dest);
+	printf("%ld\n",strlen(src));
+	printf("%c\n", dest[0]);
+	printf("%c\n", dest[1]);
+	printf("%c\n", dest[2]);
 }
